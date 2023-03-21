07ccf153568ddb35b8ee3b3e81f68bec8b4cf54c
aag <aag@novatec-gmbh.de>
2023-03-21 09:03:56 +0100

    Changed alert for Clamav

--- roles/clamav/templates/clamav.sh
+++ roles/clamav/templates/clamav.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-/usr/bin/clamscan -i -r /home --log="/var/log/clamav/clamscan-$(date +\%Y-\%m-\%d).log" >/dev/null 2>&1
-        && notify-send "ClamAV analisis is done. Check the log in var/log/clamav/clamscan"
+/usr/bin/clamscan -i -r /home --log="/var/log/clamav/clamscan-$(date +\%Y-\%m-\%d).log"
+
+line=$(head -n 1 filename)
+
+if [ $line -ne "---------------------" ]; then
+do 
+        cp /var/log/clamav/clamscan-$(date +\%Y-\%m-\%d).log /home/*/Desktop/clamav/clamscan-$(date +\%Y-\%m-\%d).log
+        notify-send "ClamAV analisis is done. Check the log in Desktop/clamav/clamscan"
+fi
+
 
 exit 0
