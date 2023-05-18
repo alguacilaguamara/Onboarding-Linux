e71f5bee2e07d3acf16adc2b1e9ca61576e453b3
aag <aag@novatec-gmbh.de>
2023-05-18 15:11:26 +0200

    Changed ClamAV cron

--- roles/clamav/templates/clamav.sh
+++ roles/clamav/templates/clamav.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-/usr/bin/clamscan -i -r /home --log="/var/log/clamav/clamscan-$(date +\%Y-\%m-\%d).log"
+filename="/var/log/clamav/clamscan-$(date +\%Y-\%m-\%d).log"
 
-line=$(head -n 1 filename)
+# Update database of ClamAV
+/usr/bin/freshclam
+#Run the scanner
+/usr/bin/clamscan -i -r /home --log=$filename >/dev/null 2>&1
+
+line=$(head -n 1 $filename)
 
 if [ $line -ne "---------------------" ]; then
 do 
-        cp /var/log/clamav/clamscan-$(date +\%Y-\%m-\%d).log /home/*/Desktop/clamav/clamscan-$(date +\%Y-\%m-\%d).log
+        cp $filename /home/*/Desktop/clamav/clamscan-$(date +\%Y-\%m-\%d).log
         notify-send "ClamAV analisis is done. Check the log in Desktop/clamav/clamscan"
 fi
 
